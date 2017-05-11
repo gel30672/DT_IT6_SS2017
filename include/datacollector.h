8f08e029d7e025536549576049c5060e2b3f7438
Martin Pfistermeister <m.pfistermeister@t-online.de>
2017-05-11 14:46:45 +0200

    Adaptions

---
+++ include/datacollector.h
@@ -0,0 +1,23 @@
+//
+// Created by pfm on 5/11/17.
+//
+
+#ifndef LOCSRV_DATACOLLECTOR_H
+#define LOCSRV_DATACOLLECTOR_H
+
+#include <xls/libxl.h>
+
+
+class datacollector {
+public:
+
+    datacollector();
+    ~datacollector();
+    void run();
+
+private:
+
+};
+
+
+#endif //LOCSRV_DATACOLLECTOR_H
---
+++ src/datacollector.cpp
@@ -0,0 +1,42 @@
+//
+// Created by pfm on 5/11/17.
+//
+
+#include "../include/datacollector.h"
+#include <string>
+#include "../include/ReadDataFromSerial.h"
+
+
+datacollector::datacollector() {
+
+}
+
+datacollector::~datacollector() {
+
+}
+
+void datacollector::run() {
+    libxl::Book* book = xlCreateBookA();
+    std::string label = "Data";
+    ReadDataFromSerial *reader = new ReadDataFromSerial();
+    input buf;
+
+
+    book->load("/home/pfm/Documents/data.xls");
+    if(book){
+        libxl::Sheet* sheet = book->addSheet(label.c_str(), 0);
+
+        for(int i = 3; i < 20; i++){
+            reader->GetTestData(&buf, 5);
+            //std::cout << buf.A << " " << buf.B << " " << buf.C << std::endl;
+            sheet->writeStr(i,3, std::to_string(buf.A).c_str());
+            sheet->writeStr(i,4, std::to_string(buf.B).c_str());
+            sheet->writeStr(i,5, std::to_string(buf.C).c_str());
+
+        }
+
+
+        book->save("/home/pfm/Documents/data.xls");
+        book->release();
+    }
+}
