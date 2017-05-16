440856b19928535afc93e8bbb2cae087ffbbe2a2
RacingPhil69 <p.winand@gmx.de>
2017-05-16 15:01:47 +0200

    sds

--- 02_Implementierung/ISR/isr.h
+++
@@ -1,9 +0,0 @@
-#ifndef ISR_H_
-#define ISR_H_
-
-void isr_init();
-
-/* rotary sensor */
-void isr_gpio_pin27();
-
-#endif
--- 02_Implementierung/PROJECT_ADAPTER/project_adapter.h
+++
@@ -1,6 +0,0 @@
-#ifndef PROJECT_ADAPTER_H_
-#define PROJECT_ADAPTER_H_
-
-typedef enum {E_OK, E_NOT_OK} e_state;
-
-#endif
