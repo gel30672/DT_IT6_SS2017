ff648a48881dd77ebf065dd7ef548521974bd123
SDzinklandi <zinkl.andreas@googlemail.com>
2017-05-28 16:47:55 +0200

    removed unused define

--- include/DeviceConfiguration.h
+++ include/DeviceConfiguration.h
@@ -39,7 +39,6 @@
 //TODO THIS IS JUST FOR LATER MERGE PURPOSE
 //This defines the needed changes or calls to functions in other modules
 #define GET_CURRENT_DRIVEN_DISTANCE 10.0
-#define GET_POSITION_FROM_UWB(position) (*position.x=4;*positiony=4;)
 #define CALL_CAR_WHEEL_DEGREES(x) SteerDegrees(x);
 #define CALL_CAR_MOTOR_SPEED(x) MotorMoveKph(x);
 
