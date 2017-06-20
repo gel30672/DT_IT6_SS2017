f53152d5182f494b3f3fb046ff32e431e30b1f35
SDzinklandi <zinkl.andreas@googlemail.com>
2017-06-20 14:27:34 +0200

    update

--- src/libmdrv/MAIN/main.cpp
+++ src/libmdrv/MAIN/main.cpp
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <wiringPi.h>
 #include <wiringSerial.h>
+#include <pthread.h>
 
 #include <errno.h>
-
-#include "../../../include/RouteDriver.h"
+#include "../../../include/Vector.h"
+#include "../../../include/CarSystem.h"
+#include "../../../include/CarCommandTestInterface.h"
 
 extern "C"{
 #include "../GPIO/gpio.h"
@@ -17,7 +19,9 @@ int last_input;
 
 void main_init()
 {
+	std::cout << "TEST 1" << std::endl;
 	gpio_init();
+	std::cout << "TEST 2" << std::endl;
 	distanceSinceStart = 0;
 
 	/* Wmotor pid params */
@@ -25,14 +29,18 @@ void main_init()
 	motor_pid.ki = 0.002;//0.001
 	motor_pid.kd = 0.0008;//0.0006
 
-	motor_pid.out_max = 100; //~70% of maximum
-	motor_pid.out_min = -100;
+	motor_pid.out_max = 70; //~70% of maximum
+	motor_pid.out_min = -70;
 
 	/* init hardware pwm for steering servo */
+	std::cout << "TEST 3" << std::endl;
 	HwPwmSteeringInit();
+	std::cout << "TEST 4" << std::endl;
 
 	/* init motor */
+	std::cout << "TEST 5" << std::endl;
 	motorInit();
+	std::cout << "TEST 6" << std::endl;
 
 	e_driving_state = RAMP;
 }
@@ -95,11 +103,11 @@ void vehicle_state_machine()
 
 void task_100_ms()
 {
-	std::cout << "HUHU" << std::endl;
 	/* rotations / 2700ms */
 	shaft_rotations = serialGetchar(uart);
 	/*/2.7 /10 == ration/100ms; *6.666 == ration of wheel */
 	/*in cm*/
+
 	distanceSinceStart += ((shaft_rotations/2.7) /10) * 6.735; //6.83
 	serialFlush(uart);
 
@@ -121,36 +129,89 @@ void task_100_ms()
 	/* pid controller rpm */
 	calc_pid(&motor_pid);
 
-	printf("out: %d, state: %d, sp: %d, rpm %d, dist: %lf, err: %d, rot: %d \n", motor_pid.out,
-			e_driving_state, motor_pid.actual_setpoint, motor_pid.in, distanceSinceStart, motor_pid.old_err, shaft_rotations);
+	//printf("out: %d, state: %d, sp: %d, rpm %d, dist: %lf, err: %d, rot: %d \n", motor_pid.out,
+	//		e_driving_state, motor_pid.actual_setpoint, motor_pid.in, distanceSinceStart, motor_pid.old_err, shaft_rotations);
 }
 
-int main()
-{
-	main_init();
+void *testCommands(void*) {
+
+	CarCommandTestInterface* testing  = new CarCommandTestInterface();
+	testing->runTest();
+
+}
+
+void *callRouteCalculation(void *) {
 
 	//Route Driver Initialization
-	Map *map = new Map();
-    RouteDriver *rtedrv = new RouteDriver(map);
-	short lastErrorCode = 0;
+	CarSystem* carSystem = new CarSystem();
 
-	int old_time = 0;
-	int time = 0;
-    while(1)
-    {
-    	time = millis();
-    	if(time >= old_time + 100)
-    	{
-        	old_time = time;
-        	task_100_ms();
-    	}
-		short status = rtedrv->checkDrive();
+    // Error code history
+    short lastErrorCode = SUCCESS;
+
+	// We drove the init track
+	if(PRINT_ERROR_CODE) std::cout << "MAIN - CALL INIT CAR SYSTEM START" << std::endl;
+	short errCode = carSystem->initialize();
+    if(PRINT_ERROR_CODE) std::cout << "MAIN - CALL INIT CAR SYSTEM (" << errCode << ")" << std::endl;
+
+	// Now we can check continuously our drive
+	std::cout << "CARSYSTEM: RUN" << std::endl;
+	while(1) {
+		short status = carSystem->run();
 		if(PRINT_ERROR_CODE && status != lastErrorCode) {
 			std::cout << "checkdrive(" << status << ")" << std::endl;
 			lastErrorCode = status;
 		}
+	}
+}
 
+void *car(void*) {
+
+	// Run the car
+	int old_time = 0;
+	int time = 0;
+	while(1) {
+
+		time = millis();
+		if(time >= old_time + 100)
+		{
+			old_time = time;
+			task_100_ms();
+		}
+	}
+}
+
+int main() {
+    short threadCount = 2;
+    pthread_t threads[threadCount];
+    int rc_1;
+    int rc_2;
+
+
+	std::cout << "TEST XX" << std::endl;
+    main_init();
+
+	std::cout << "Using Testsystem? (y/n) " << std::endl;
+	bool useTestSystem = false;
+	string val;
+	std::cin >> val;
+
+	if(val == "y" || val == "Y") useTestSystem = true;
+
+	if(useTestSystem) {
+		rc_1 = pthread_create(&threads[0], NULL, testCommands, (void*) NULL);
+	} else {
+		rc_1 = pthread_create(&threads[0], NULL, callRouteCalculation, (void *) NULL);
+	}
+    rc_2 = pthread_create(&threads[1], NULL, car, (void *) NULL);
+
+    if (rc_1){
+        cout << "Error:unable to create thread route," << rc_1 << endl;
+        exit(-1);
+    }
+    if (rc_2){
+        cout << "Error:unable to create thread car," << rc_2 << endl;
+        exit(-1);
     }
 
-    return 0;
+    pthread_exit(NULL);
 }
